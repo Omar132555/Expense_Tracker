d141fe678ab008133fc67bf182f68e8db9f97d85
Omar <omar.mostafa1325@gmail.com>
2025-04-14 16:59:11 +0200

    V1

---
+++ Register_ex.php
@@ -0,0 +1,29 @@
+<?php
+require_once 'Connection.php';
+require 'vendor/autoload.php';
+require_once 'Functions.php';
+use Firebase\JWT\JWT;
+
+$username = filter_input(INPUT_POST, 'username', FILTER_DEFAULT);
+$password = filter_input(INPUT_POST, 'password', FILTER_DEFAULT);
+if(!$username || !$password) {
+    echo "Username and password Format are inValid!";
+    $error = true;
+    exit;
+}
+if(!$error)
+{
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+    $stmt = $connection->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
+$stmt->bind_param("ss", $username, $hashed_password);
+if($stmt->execute()){
+    echo "Registration successful!";
+} else {
+    echo "Error: " . $stmt->error;
+}
+$stmt->close();
+}
+else{
+    echo "Error: ";
+}
+?>
